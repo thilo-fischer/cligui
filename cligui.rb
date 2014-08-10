622805e688abaf753dfd118bf623a075f7088f28
Thilo Fischer <thilo-fischer@gmx.de>
2014-08-10 09:00:06 +0000

    knows how to parse category, program and preset files (preset categories not yet ...)

--- cligui.rb
+++ cligui.rb
@@ -7,199 +7,160 @@
   This program is licenced under GPLv3.
 =end
 
-require 'gtk2'
+#require 'gtk2'
 require 'rexml/document'
 
-class CliDefTree
-
-  def initialize(dirname)
-    @tree = parse_clidefs(dirname, Category.new("top", "root element of the tree (invisible to the user)"))
-  end
-
-def parse_clidefs(dirname, category)
-  Dir.foreach(dirname) { |filename|
-    if filename == "." or filename == ".."
-      next
-    elsif File.directory?(filename)      
-      category.add_subcategory Category.read(File.join(dirname, filename))
-    elsif File.file?(filename)
-      category.add_child CliDef.read(File.join(dirname, filename))
-    else
-      puts "warning: ..." # todo
-    end
-  }
-end # parse_clidefs
-
-def parse_clidef(filename)
-  listener = MyListener.new 
-  file = File.new filename
-  REXML::Document.parse_stream(file, listener)
-end # parse_clidef
-
-end # class CliDefs
-
-# call function on dir
-# classify dir
-# create object representing dir
-# call function recursively on subdirs and add return values to list
-# 
 
 class CliDef
 
-  @CHILD_CLASSES = [ Category, Program ]
-  @BASENAME = "clidef.xml"
+  #CHILD_CLASSES = [ Category, Program ]
+  #BASEFILENAME = "clidef.xml"
 
   def initialize(directory)
     @directory = directory
     @children = {}
-    @CHILD_CLASSES,each { |c| @children[c] = {} }
+    self.class::CHILD_CLASSES.each { |c| @children[c] = [] }
+    puts "new " + self.class.to_s + " object: " + inspect
   end
 
-  # return new object if directory corresponds to this class, return nil otherwise
-  def self.create_if_valid(dirname)
-    filename = File.join(dirname, @BASENAME)
+  # return new object if given path corresponds to this class, return nil otherwise
+  def self.create_if_valid(path)
+    filename = File.join(path, self::BASEFILENAME)
     if File.exist?(filename)
-      obj = new(dirname) 
-      obj.read_deffile(filename)
+      obj = new(path) 
+      obj.read_selffile(filename)
+      obj
     end
   end
 
-  def read_deffile(filename)
-    self
+  def gather_children
+    gather_children_from_directory(@directory, self.class::BASEFILENAME)
   end
 
-  def read_directory
+private
+
+  def gather_children_from_directory(directory, *entries_to_skip)
     Dir.foreach(directory) do |entry|
-      if entry == "." or entry == ".."
+      if entry == "." or entry == ".." or entries_to_skip.include?(entry)
         next
       end
-      path = File.join(directory, entry)
-      if File.directory?(path)      
-        read_subdirectory(path)
-      elsif File.file?(path)
-        read_file(path) if entry != @BASENAME
-      else
-        puts "warning: ..." # todo
-      end
+      add_child_from_path(File.join(directory, entry))
     end
   end
 
-  def read_subdirectory(dirname)
-    @CHILD_CLASSES,each do |cls|
-      obj = cls.create_if_valid(dirname)
-      if obj
-        add_child(cls, obj)
-        obj.read_directory
-        return obj
+  def add_child_from_path(path)
+   self.class::CHILD_CLASSES.each do |cls|
+      child = cls.create_if_valid(path)
+      if child
+        add_child(cls, child)
+        child.gather_children
+        return child
       end
     end
-  end
-
-  def read_file(path)
-    puts "warning: ignoring file " + path
+    puts "warning: ... " # TODO
   end
 
   def add_child(key, value)
     @children[key] << value
+    puts self.to_s + " got new child: " + value.inspect
   end
 
 end # class CliDif
 
-#class CliDef
-#
-#  def read(dirname)
-#    [Category, Program].each do |clazz|
-#      filename = File.join(dirname, clazz.BASEFILENAME)
-#      if File.exist?(filename)
-#        add_child(clazz, clazz.new(filename))
-#        break
-#      end
-#    end
-#  end
-#
-#  def add_child()
-#  end
-#
-##  def self.process_dir(dirname)
-##    Dir.foreach(dirname) do |entry|
-##      if entry == "." or entry == ".."
-#        next
-#      end
-#      path = File.join(dirname, entry)
-#      if File.directory?(path)      
-#        process_dir( Category.read(path)
-#      elsif File.file?(path)
-#        category.add_child CliDef.read(path)
-#      else
-#        puts "warning: ..." # todo
-#      end
-#    end
-#  end
-#
-#end # class CliDef
+class Category < CliDef; end
+class Program  < CliDef; end
+class Preset   < CliDef; end
+class PresetCategory < Category; end
 
 class Category < CliDef
 
-  @BASEFILENAME = "category.xml"
+  CHILD_CLASSES = [ Category, Program ]
+  BASEFILENAME = "category.xml"
 
-  def initialize(name, description)
-    @name = name
-    @description = description
-    @subcategories = []
-    @programs = []
-  end
-
-  def self.process_dir(dirname, parent)
-    this = read(dirname) 
-    parent.add_subcategory(this)
-    Dir.foreach(dirname) do |entry|
-      if entry == "." or entry == ".."
-        next
-      end
-      path = File.join(dirname, entry)
-      if File.directory?(path)      
-        process_dir(path, this)
-      end
-    end
-  end
-
-  def add_subcategory(sub)
-    @subcategories << sub
-  end
-
-  def add_program(program)
-    @programs << program
+  def read_selffile(filename)
+    # TODO verify document validity using DTD file and ignore invalid files
+    doc = REXML::Document.new File.new(filename)
+    root = doc.root
+    @title       = root.elements["title"].text
+    @description = root.elements["description"].text
   end
 
 end # class Category
 
+
 class Program < CliDef
 
+  CHILD_CLASSES = [ Preset, PresetCategory ]
   BASEFILENAME = "cli.xml"
 
-  def initialize()
-    @presets = []
+  def read_selffile(filename)
+    # TODO verify document validity using DTD file and ignore invalid files
+    doc = REXML::Document.new File.new(filename)
+    root = doc.root
+    @description = root.elements["description"].text
+    @executable  = root.elements["executable"].text
+    titleelement = root.elements["title"]
+    if titleelement
+      @title = titleelement.text
+    else
+      @title = @executable
+    end
+    doc.elements.each("command/section") { |e| add_section(e) }
   end
 
-  def self.read(dirname)
-    doc = REXML::Document.new File.new(File.join(dirname, BASEFILENAME))
-    name = doc.
-    description = doc.
-    new(name, description)
+  def gather_children
+    gather_children_from_directory(File.join(@directory, Preset::DIRNAME), self.class::BASEFILENAME)
   end
 
-  def add_preset(preset)
-    @presets << preset
+  def add_section(e)
+    puts "TODO: add section"
   end
 
 end # class Program
 
+
 class Preset < CliDef
 
-  def self.read(filename)
+  CHILD_CLASSES = []
+  DIRNAME = "presets"
+
+  def read_selffile(filename)
+    # TODO verify document validity using DTD file and ignore invalid files
+    doc = REXML::Document.new File.new(filename)
+    root = doc.root
+    @title       = root.elements["title"].text
+    @description = root.elements["description"].text
+    doc.elements.each("preset/argument") { |e| add_argument(e) }
   end
 
-end # cliss Preset
+  def self.create_if_valid(path)
+    obj = new(path) 
+    obj.read_selffile(path)
+    obj
+  end
+
+  def gather_children
+    nil
+  end
+
+  def add_argument(e)
+    puts "TODO: add argument"
+  end
+
+end # class Preset
+
+
+class PresetCategory < Category
+  CHILD_CLASSES = [ Preset, PresetCategory ]
+end # class PresetCategory
+
+
+clidef_root = Category.new("./cli-definitions")
+clidef_root.gather_children
+puts "====="
+puts clidef_root.inspect
+
+__END__
 
 window = Gtk::Window.new
 
