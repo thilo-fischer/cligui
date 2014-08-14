7238c7a55b4ee17fc46e05fe2edf26869f512477
Thilo Fischer <thilo-fischer@gmx.de>
2014-08-14 18:06:36 +0000

    help_to_clidef smoke-tested successfully

--- help_to_clidef.rb
+++ help_to_clidef.rb
@@ -9,8 +9,11 @@
 
 #require '...'
 require 'rexml/element'
+require 'rexml/document'
 
-$root = REXML::Element.new("category")
+# FIXME is document necessary?
+doc = REXML::Document.new("<category></category>")
+$root = doc.root
 
 command = ARGV.join(" ")
 lines = `#{command}`.lines
@@ -22,7 +25,9 @@ class StateExpectUsage < StateExpectDescription; end
 class StateExpectUsage < StateExpectDescription
   REGEXP = /^Usage:\s+(\w+)\s+(.*)$/
   def self.parse_line(ln)
+    puts "*** #{self.to_s}.parse_line(\"#{ln}\")"
     if ln =~ REGEXP
+      puts "  * #{Regexp.last_match.inspect}"
       executable = Regexp.last_match(1)
       puts "warning ..." if executable != ARGV[0]
       e = $root.add_element("title")
@@ -53,34 +58,49 @@ class StateExpectUsage < StateExpectDescription
       end
       StateExpectDescription
     else
-      super
+      superclass.parse_line(ln)
     end
   end
 end # class StateExpectUsage 
 
 class StateExpectDescription < StateParseOptions
   def self.parse_line(ln)
-    if ln !~ super.class.REGEXP
-      e = $root.elements["description"]
+    puts "*** 01 #{self.to_s}.parse_line(\"#{ln}\")"
+ if ln !~ superclass::REGEXP
+      puts "  * desc #{Regexp.last_match.inspect}"
+      e   = $root.elements["description"]
       e ||= $root.add_element("description")
       e.add_text ln
       self
     else
-      super
+      puts "  * opts #{Regexp.last_match.inspect}"
+      superclass.parse_line(ln)
     end
   end
 end
 
 class StateParseOptions
-  REGEXP = /^\s*((-?\w+)(\s+|\s*,\s*))(.*)$/
+  REGEXP = /^\s*((?:-[\w\-]+(?:\s+|\s*[,=]\s*))+)(.*?)$/
   def self.parse_line(ln)
+    puts "*** 02 #{self.to_s}.parse_line(\"#{ln}\")"
     # assume options are always the first thing after the command name in the "Usage: ..." line
-    options = $root.elements["section"]
-    $root.add_element("section", {"title"=>"options"}) unless options
+    unless @opt_section_e
+      @opt_section_e   = $root.elements["section"]
+      @opt_section_e ||= $root.add_element("section", {"title"=>"options"})
+    end
     if ln =~ REGEXP
-      e = options.add_element("flag") # todo switch?
-      names = Regexp.last_match(2)
-      names.split(/\s+|\s*,\s*/).each do |name|
+      puts "  * #{Regexp.last_match.inspect}"
+      names = Regexp.last_match(1)
+      desc  = Regexp.last_match(2)
+      if names =~ /=\s*/
+        e = @opt_section_e.add_element("flag")
+        flag_arg = desc.slice!(/[\w\-\.:]+(\s+|$)/).rstrip
+        sect = e.add_element("section", {"title"=>flag_arg,"count"=>"1"})
+        sect.text = ""
+      else
+        e = @opt_section_e.add_element("switch")
+      end
+      names.split(/\s+|\s*[,=]\s*/).each do |name|
         if name =~ /^-\w$/
           n = e.add_element("shortname")
         else
@@ -88,11 +108,13 @@ class StateParseOptions
         end
         n.text = name
       end
+
       d = e.add_element("description")
-      d.text = Regexp.last_match(4)
+      d.text = desc
+      @recent_e = e
     else
       # append to desription of most recent option
-      options.elements[-1].elements["description"].add_text ln
+      @recent_e.elements["description"].add_text ln
     end
     self
   end
@@ -101,11 +123,11 @@ end
 state = StateExpectUsage
 
 until lines.empty? do
-  ln = lines.shift
+  ln = lines.shift.chomp
+  puts "* #{state.to_s} << \"#{ln}\""
   state = state.parse_line(ln)
 end
 
-$root.write(STDOUT)
+$root.write(STDOUT, 2)
+STDOUT.puts
 
-
-        
